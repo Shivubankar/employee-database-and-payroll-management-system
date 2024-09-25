64b0b0b89cc8b1c76816c0dada9dbc87c2a4d9c5
Shivakumar S H <156896979+Shivubankar@users.noreply.github.com>
2024-09-25 14:50:38 +0530

    Add files via upload

---
+++ EMPLOYEE-DATABASE-AND-PAYROLL-MANAGEMENT-SYSTEM-master/README.md
@@ -0,0 +1,28 @@
+# EMPLOYEE-DATABASE-AND-PAYROLL-MANAGEMENT-SYSTEM
+"Employee Database And Payroll Management System" is designed to make the existing manual system automatic with the help of computerised equipment and full-edged computer software, fulfilling their requirements, so that their valuable data and information can be stored for a longer period with easy access and manipulation of the same. The required software is easily available and easy to work with. This web application can maintain and view computerised records without getting redundant entries. The project describes how to manage user data for good performance and provide better services for the client.
+
+# software required for the development of this project is:
+### 1.software : XAMPP
+### 2.Front End : HTML,CSS
+### 3.Programming language : PHP
+### 4.Database Environment : MYSQL and PhpMyAdmin
+### 5.Server : APACHE 
+
+# Screenshot
+## ADMIN LOGIN PAGE
+![LOG in page](https://user-images.githubusercontent.com/102343556/211178907-b5a03b7f-cedb-49bf-a7a0-fbdeb6128cd4.png)
+## HOME PAGE
+![HOME page](https://user-images.githubusercontent.com/102343556/211179011-efb5ee87-4ed0-4f5e-b8f7-e2bb38bdf7a2.png)
+## DEPARTMENT DATA
+![Department data](https://user-images.githubusercontent.com/102343556/211178921-893e07d2-36a1-4275-9976-2f213cdd49ef.png)
+## EMPLOYEE DATA
+![Employee data](https://user-images.githubusercontent.com/102343556/211178941-bb8a0645-5aef-4a6a-90aa-24c1de4ab51b.png)
+## PAYMENTS
+![PAYMENT](https://user-images.githubusercontent.com/102343556/211178954-1c12508a-8c88-4ace-9ec6-6749ea0a354d.png)
+## EMPLOYEE PAYSLIP
+![Employewe payslip](https://user-images.githubusercontent.com/102343556/211178964-e21c0e50-9570-4025-b4bf-10847cf76d2f.png)
+## SET SALARY
+![Set salary](https://user-images.githubusercontent.com/102343556/211178981-afffaf9a-a500-4df4-b0f2-db9a3a986938.png)
+## PAYMENT HISTORY
+![Payment history](https://user-images.githubusercontent.com/102343556/211178998-024a4b20-4948-412d-ac12-15fd8af23020.png)
+
---
+++ EMPLOYEE-DATABASE-AND-PAYROLL-MANAGEMENT-SYSTEM-master/index.php
@@ -0,0 +1,123 @@
+<!doctype html>
+<html>
+
+<head>
+	<meta charset="utf-8">
+	<title>Welcome to homepage</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+
+	<div class="loginbox">
+		<h2>Log in </h2>
+		<form method="post" action="newfile.php">
+			<p>Username</p>
+			<input type="text" name="username" placeholder="admin">
+			<p>Password</p>
+			<input type="password" name="password" placeholder="">
+			<input type="submit" name="submit" value="Submit">
+		</form>
+	</div>
+</body>
+
+</html>
+<!-- <!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Home page</title>
+	<link rel="stylesheet" type="text/css" href="style2.css">
+	<link rel="preconnect" href="https://fonts.gstatic.com">
+	<link href="https://fonts.googleapis.com/css?family=josefin+sans:ital,wght@,100;0,200;0,300;0,400;0,500;0,700;1,300;1,400;1,600;1,700&display=swap" rel="stylesheet">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+</head>
+
+<body>
+	<div class="hero">
+		<nav>
+			<h2 class="logo">EMPLOYEE PLAYROLL MANAGEMENT<span>System</span></h2>
+			
+		</nav>
+		<div class="content">
+			<h4>Hello,</h4>
+			<h1>PAY<span></span></h1>
+			<h3>Student login</h3>
+			<div class="newslatter">
+				<form method="post" action="newfile.php">
+					<input type="text" name="email" id="mail" placeholder="Enter you user id">
+					<br>
+
+				</form>
+				<form method="post" action="newfile.php">
+					<input type="password" name="password" id="mail" placeholder="Enter password">
+					<input type="submit" name="submit" value="Submit">
+				</form>
+			</div>
+		</div>
+	</div>
+
+	<!-- <section class="about">
+            <div class="main">
+                <img src="pho.jpeg">
+                <div class="about-text">
+                    <h2><span>About </span>Us</h2>
+                    <h5>Kite</h5>
+                     <p>We provide Best notes from heighely experienced professor</p>
+                        <a href="#"><button type="button">Contact us</button></a>
+                    
+                </div>
+            </div>
+        </section>
+        
+        <div class="service">
+            <div class="title">
+                <h2>Our services</h2>
+            </div>
+            <div class="box">
+                <div class="card">
+                    <i class="fa-solid fa-bars"></i>
+                    <div class="pra">
+                        <p> Teachers update latest notes which can be downloaded for free
+                        </p>
+                        <p style="text-align: center;">
+                            
+
+                        </p>
+                    </div>
+                </div>
+
+                <div class="card">
+                    <i class="fa-solid fa-user"></i>
+                    <div class="pra">
+                        <p>We have highely experienced lecturers qualified from top institutes all over India
+                        </p>
+                        <p style="text-align: center;">
+                        </p>
+                    </div>
+                </div>
+
+                <div class="card">
+                    <i class="fa-solid fa-bell"></i>
+                    <div class="pra">
+                        <p>We give notefications as soon as lecturer post notes
+                        </p>
+                        <p style="text-align: center;">
+                            <p style="align-items: center;"></p>
+                           
+
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div> --> -->
+
+
+</body>
+
+</html>
